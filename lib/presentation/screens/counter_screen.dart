bcc4b1ad5d82da43a8ed374b69f256e796726140
Davichobits <davidruizdeveloper@gmail.com>
2024-04-17 18:43:59 -0500

    Changing state from widget

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,27 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Center(child: Text('Counter'))),
-        body: const Center(
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                '10',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+                '$clickCounter',
+                style:
+                    const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
               ),
-              Text('Clicks')
+              const Text('Clicks')
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            setState(() {
+              clickCounter++;
+            });
+          },
           child: const Icon(Icons.plus_one),
         ));
   }
